8143493efbe7275b057016b82fbc23e4ea6928a7
Yasmeen Hany <yasmeen.abdelhafiz@gmail.com>
2018-11-23 18:52:02 +0200

    initial commit: project setup, element ui setup, dropdown menu

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <DropDownMenu></DropDownMenu>
+  </div>
+</template>
+
+<script>
+  /* eslint-disable no-trailing-spaces */
+
+  import DropDownMenu from '@/components/DropDownMenu.vue';
+  export default {
+    name: 'App',
+    components: {
+      DropDownMenu
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/DropDownMenu.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <el-dropdown>
+  <span class="el-dropdown-link">
+    Dropdown List<i class="el-icon-arrow-down el-icon--right"></i>
+  </span>
+    <el-dropdown-menu slot="dropdown">
+      <el-dropdown-item>Action 1</el-dropdown-item>
+      <el-dropdown-item>Action 2</el-dropdown-item>
+      <el-dropdown-item>Action 3</el-dropdown-item>
+      <el-dropdown-item >Action 4</el-dropdown-item>
+      <el-dropdown-item >Action 5</el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
+
+</template>
+
+<script>
+  export default{
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #409EFF;
+  }
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
+</style>
